9b074a1df0b0f986512a988fc72d7347764cac3b
neo-astro <estevanabril@hotmail.es>
2022-06-28 19:00:58 -0500

    prueba

---
+++ index.js
@@ -0,0 +1,10 @@
+//imports
+const express = require('express')
+//server
+const app = express()
+const server = app.listen(5000, function () {
+  console.log('server in port:', 5000)})
+
+app.get('/',(req, res) => {
+  res.send('hola')
+})
